611b5b801064ab23c59d181ea3a1ede89a042547
anandshah7 <34805377+anandshah7@users.noreply.github.com>
2024-04-29 17:16:37 -0700

    Removed unwanted Code

--- WebAuth/obj/Debug/net8.0/WebAuth.AssemblyInfo.cs
+++ WebAuth/obj/Debug/net8.0/WebAuth.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAuth")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27431f4ac0f431e8351c9e1d0042256e118f9ba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f4bb59a98cfde1be3efaf23f4765907d52b4371")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAuth")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAuth")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
